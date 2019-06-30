b721bbddcce841db65100fc3e3b788e862b1ff68
David Charles <ratlover03@gmail.com>
2019-06-30 21:33:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -29,7 +29,6 @@ class Application
     elsif req.path.match(/add/)
       add_item = req.params["item"]
       resp.write handle_add(add_item)
-
     else
       resp.write "Path Not Found"
     end
